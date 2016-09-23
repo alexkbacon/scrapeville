2163cc7736ba58b46133cea474b1f27a1fcd3425
Alex Bacon <bakna1@hotmail.com>
2016-09-23 13:24:53 +0000

    newsville

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923104548) do
+ActiveRecord::Schema.define(version: 20160923125903) do
 
   create_table "newsvilles", force: :cascade do |t|
+    t.string   "title_url"
     t.string   "title"
     t.text     "body"
     t.string   "time"
